d98a7a4980ab75ccb1b59d7283ab6d4e424efb8e
idodo-cyber <80988028+idodo-cyber@users.noreply.github.com>
2021-03-26 15:04:13 +0300

    starting to see the end
    based on the changes made the code is capable of runing a quiz with two players and tell who won howe much points and more

--- Host.py
+++ Host.py
@@ -1,12 +1,14 @@
 import socket
 import threading
 from Client_class import Client
-SIP = "10.100.102.114"
+SIP = "10.100.102.12"
 IP = "0.0.0.0"
 CLINET_ARR = []
 SPORT = 55368
 MPORT = 53476
 lock = threading.Lock()
+ANS_WORTH = 100
+ANS_TIME = 60000
 def host():#creates the host by getting the pin an connecting the cllinets
     k = get_pin()#gets pin
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -26,8 +28,10 @@ def host():#creates the host by getting the pin an connecting the cllinets
 
     k.send("bye".encode())
     k.close()#closes connection with server
-    for i in CLINET_ARR:
-        print(i.To_string())
+    handle_quiz()#handles the quiz
+
+
+
 
 
 
@@ -50,12 +54,61 @@ def connect_client(c,addr):#connects client and sets him up with the clients nic
     print(name)
     c.send("good".encode())
 
+def handle_quiz():
+    with open("demo_quiz") as file:
+        first_lines = "".join([file.readline() for _ in range(5)]).split("\n")
+        while not first_lines == [""]:
+            print(first_lines)
+            for i in first_lines:
+                if "_T" in i:
+                    ans = i.split("_")
+                    arr = []
+                    for cli in CLINET_ARR:
+                        thread = threading.Thread(target=handle_ans,args=(ans[1], cli))  # creates new thread for client
+                        thread.start()
+                        arr.append(thread)
+                    for thr in arr:
+                        thr.join()#waits until all clients are finished
+                    CLINET_ARR.sort(key = lambda x:x.value,reverse=True)#sorts the clients based on their value
+                    for i in CLINET_ARR:
+                        i.socket.send(build_ans(str(i.value) + "_" + str(i.added_value) + "_"+str(CLINET_ARR.index(i)+1)).encode())
+                        #sends the value,added value and place of client to client to the client
+
+            first_lines = "".join([file.readline() for _ in range(5)]).split("\n")
+
+
+
+
 def name_in(name):#checks if the given client object has a name similer to a name prevoiusly taken
     for i in CLINET_ARR:
         if name == i.Get_name():
             return False
     return True
+def all_mesage(sock):#recievs all of the message based on the message length given at the begining of the messsage
+    lent = sock.recv(1).decode()
+    while "_" not in lent:
+        lent += sock.recv(1).decode()
+    print(lent)
+    lent = int(lent[:-1])#recives the message length
+    ans = sock.recv(lent).decode()
+    print(ans)
+    while not len(ans) == lent:
+        ans += sock.recv(lent)
+    return ans#recieves the message
 
+def build_ans(ans):
+    return str(len(ans))+"_"+ans
+
+def handle_ans(true,cli):#handles the given ans and adds points accordingly
+    cli.strt_classes()#sends start message to client
+    ans,time = cli.recv_ans()#recieves answer and time it tokk to answer from client
+    if ans == true:#if answer is correct adds the corresponding value
+        val = 1-(int(time)/ANS_TIME/2)
+        val = val*ANS_WORTH
+    else:
+        val = 0
+    cli.add_value(val)
+    cli.Set_added_value(val)
 
 
 
--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import socket
-SIP = "10.100.102.114"
+import datetime
+SIP = "10.100.102.12"
 SPORT = 55368
 MPORT = 53476
 
@@ -8,11 +9,16 @@ def client():#main loop of the client
    while  ip == None:
        ip = get_addr()
    s = cnct_client(ip)
-   while True:
-       ans = input()
-       if ans == "bye":
-           s.close()
-           break
+   ans = ""
+   while not ans == "STP":
+       ans = all_mesage(s)
+       if ans == "STRT":
+            answer = build_ans()
+            s.send(answer.encode())
+       point = all_mesage(s).split("_")
+       print("you have " + point[0] + " you gained "+point[1] + "and you are at place" + point[2])
+
+
 
 
 def get_addr():#gets the ip addr based on game pin
@@ -24,6 +30,17 @@ def get_addr():#gets the ip addr based on game pin
     s.send(ans.encode())#unite both lines ointo a single message and build func at server file to break the message down
     addr = s.recv(1024).decode()
     return addr
+
+def build_ans():
+    start_time = datetime.datetime.now()
+    ans = input()#this will change to choose based on button
+    end_time = datetime.datetime.now()
+    time_diff = (end_time - start_time)
+    execution_time = time_diff.total_seconds() * 1000
+    exe = int(execution_time)
+    ans = str(len(ans + "_" + str(exe))) + "_" + ans + "_" + str(exe)#builds the answer based on protocol len_ans_time
+    return ans
+
 
 
 def cnct_client(ip):#connects client to host based on the ip gotten from server
@@ -50,5 +67,17 @@ def handle():
             client()
         else:
             print("please enter appropriate ans")
+
+def all_mesage(sock):#recievs all of the message based on the message length given at the begining of the messsage
+    lent = sock.recv(1).decode()
+    while "_" not in lent:
+        lent += sock.recv(1).decode()
+    print(lent)
+    lent = int(lent[:-1])#recives the message length
+    ans = sock.recv(lent).decode()
+    print(ans)
+    while not len(ans) == lent:
+        ans += sock.recv(lent)
+    return ans#recieves the message
 
 handle()
